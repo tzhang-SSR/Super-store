985983b6772bf900b0de85f76554ff4a32b3373d
URpineapple <zhang.te@usky.neu.edu>
2021-03-15 17:19:24 -0400

    minor updates

--- src/components/deal/index.js
+++ src/components/deal/index.js
@@ -5,8 +5,11 @@ export default function DealsPage({ items }) {
     return (
         <div className="container">
             {
-                items.map((item, index) => <ItemCard key={index} {...item} />)
+                items.length > 0
+                    ? items.map((item, index) => <ItemCard key={index} {...item} />)
+                    : <div> There are no on-sale items at this time </div>
             }
-        </div>
+        </div >
+
     );
 }
